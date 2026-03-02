cc45aabcbceae97dec6ec423715f863e71026914
eupthere <eupthere@gmail.com>
2026-03-02 19:30:08 +0900

    style: tweak DOT_SIZE and scaling parameters

--- src/components/timelineUi.ts
+++ src/components/timelineUi.ts
@@ -1,11 +1,11 @@
-export const DOT_SIZE = 14;
+export const DOT_SIZE = 10;
 export const DOT_RADIUS = DOT_SIZE / 2;
 
-export const DOT_HOVER_SCALE = 1.25;
-export const DOT_ACTIVE_SCALE = 1.2;
+export const DOT_HOVER_SCALE = 1.5;
+export const DOT_ACTIVE_SCALE = 1.5;
 export const DOT_SCALE_ANIMATION_MS = 200;
 
-export const ROW_DOT_GAP = 22;
+export const ROW_DOT_GAP = 18;
 export const ROW_SIDE_PADDING = 4;
 export const ROW_VERTICAL_PADDING = 4;
 
@@ -15,8 +15,8 @@ export const ROW_WIDTH = RIGHT_X + DOT_RADIUS + ROW_SIDE_PADDING;
 export const ROW_HEIGHT = DOT_SIZE + ROW_VERTICAL_PADDING * 2;
 export const CENTER_Y = ROW_HEIGHT / 2;
 
-export const CONNECTOR_BLEND = 0.5;
-export const CONNECTOR_HANDLE_RATE = 1.5;
+export const CONNECTOR_BLEND = 0.6;
+export const CONNECTOR_HANDLE_RATE = 2.1;
 export const CONNECTOR_MAX_DISTANCE = 40;
 
 export const ANSWER_APPEAR_DURATION_MS = 360;
